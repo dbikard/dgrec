b34ce37ef26ca493e3b6fae16c5259c098191b5f
David <dbikard@pasteur.fr>
2024-02-09 08:45:19 +0100

    added option to save umi data

--- dgrec/core.py
+++ dgrec/core.py
@@ -12,6 +12,7 @@ from collections import defaultdict, Counter
 import numpy as np
 import itertools
 import click
+import csv
 from .utils import get_mutations, mut_to_str
 
 # %% ../nbs/API/00_core.ipynb 7
@@ -23,7 +24,7 @@ def get_UMI_genotype(fastq_path: str, #path to the input fastq file
                      ) -> dict:
     
     """Takes as input a fastq_file of single read amplicon sequencing, and a reference amplicon sequence.
-       Returns a dictionnary containing as keys UMIs and as values a list of all genotype strings read for that UMI.
+       Returns a dictionnary containing as keys UMIs and as values a Counter of all genotype strings read for that UMI.
     """
     with gz.open(fastq_path,'rt') as handle:
         reads=SeqIO.parse(handle,"fastq")
@@ -48,22 +49,28 @@ def get_UMI_genotype(fastq_path: str, #path to the input fastq file
     
     log='n reads:\t{}\nn_reads pass filter:\t{}\nn_reads aligned:\t{}\n'.format(n_reads,n_reads_pass_Qfilter,n_reads_aligned)
     log+=f"Number of UMIs: {len(UMI_dict)}\n"
+    
+    UMI_gencounter={}
+    umi_readcounts=[]
+    for umi in UMI_dict:
+        umi_readcounts.append(len(UMI_dict[umi]))
+        UMI_gencounter[umi]=Counter(UMI_dict[umi])
+
+    log+=f"Median number of reads per UMI: {np.median(umi_readcounts)}"
     print(log)
-    return UMI_dict
+    return UMI_gencounter
 
 # %% ../nbs/API/00_core.ipynb 9
-def correct_UMI_genotypes(UMI_dict: dict, #the output of the get_UMI_genotype function
-                          reads_thr=2 #only keep UMIs for which we have more than reads_thr reads
+def correct_UMI_genotypes(UMI_gencounter: dict, #the output of the get_UMI_genotype function
+                          reads_thr=2 #only assign a genotype to a UMI if we have reads_thr reads for that genotype or more
                           ) -> dict:
     """Keeps only the genotype with the most reads for each UMI.
     Returns a dictionary with UMIs as keys and a tuple as value: (genotype string, number of reads)
     """
     UMI_gen_dict={}
-    for umi in UMI_dict:
-        N=len(UMI_dict[umi])
-        if N>=reads_thr: #only consider umis for which we have more than reads_thr reads
-            gen_counter=Counter(UMI_dict[umi])
-            gen=sorted(list(gen_counter.items()),key=lambda x: x[1], reverse=True)[0] #only keep the genotype with the most reads for this umi
+    for umi in UMI_gencounter:
+        gen, n =UMI_gencounter[umi].most_common(1)[0]
+        if n>=reads_thr: #only assign a genotype to a UMI if we have reads_thr reads for that genotype or more
             UMI_gen_dict[umi]=gen
 
     return UMI_gen_dict
@@ -72,7 +79,7 @@ def correct_UMI_genotypes(UMI_dict: dict, #the output of the get_UMI_genotype fu
 def genotype_UMI_counter(UMI_gen_dict):
     """Takes as input the output of correct_UMI_genotypes() and 
     returns a list of genotypes sorted by the number of UMIs detected corresponding that each genotype."""
-    umi_counter=Counter([gen for gen,n in UMI_gen_dict.values()])
+    umi_counter=Counter(UMI_gen_dict.values())
     gen_sorted_list=sorted(list(umi_counter.items()),key=lambda x: x[1], reverse=True)
     return gen_sorted_list
 
@@ -84,9 +91,16 @@ def get_genotypes(fastq_path: str, #path to the input fastq file
                     quality_threshold: int = 30, #threshold value used to filter out reads of poor average quality
                     ignore_pos: list = [], #list of positions that are ignored in the genotype
                     reads_thr: int = 0, #minimum number of reads required to take a UMI into account. Using a number >2 enables to perform error correction for UMIs with multiple reads.
+                    save_umi_data: str = None, #path to the csv file where to save the details of the genotypes reads for each UMI. If None the data isn't saved.
                     ):
     """Putting things together in a single wrapper function that takes the fastq as input and returns the list of genotypes."""
     UMI_dict = get_UMI_genotype(fastq_path, ref_seq, umi_size, quality_threshold, ignore_pos)
+    if save_umi_data:
+        with open(save_umi_data,"w", newline='') as handle: 
+            csv_writer = csv.writer(handle,delimiter="\t",doublequote=False)
+            for umi in itertools.islice(UMI_dict,20):
+                csv_writer.writerow([umi,list(UMI_gencounter[umi].items())])
+
     UMI_gen_dict=correct_UMI_genotypes(UMI_dict, reads_thr)
     gen_list = genotype_UMI_counter(UMI_gen_dict)
     print("Number of genotypes:", len(gen_list))
@@ -102,15 +116,17 @@ def get_genotypes(fastq_path: str, #path to the input fastq file
 @click.option('--quality_threshold', '-q', default=10, help="threshold value used to filter out reads of poor average quality")
 @click.option('--ignore_pos', '-i', default=[], multiple=True, help="list of positions that are ignored in the genotype, e.g. [0,1,149,150]")
 @click.option('--reads_thr', '-r', default=0, help="minimum number of reads required to take a UMI into account. Using a number >2 enables to perform error correction for UMIs with multiple reads")
+@click.option('--save_umi_data','-s', default=None, help="path to a csv file to save the details of the genotypes reads for each UMI. If None the data isn't saved.")
 @click.option('--output', '-o', default="genotypes.csv", help="output file path")
-def dgrec_genotypes(fastq, ref, umi_size, quality_threshold, ignore_pos, reads_thr, output):
+def dgrec_genotypes(fastq, ref, umi_size, quality_threshold, ignore_pos, reads_thr, save_umi_data, output):
     ref=next(SeqIO.parse(ref,"fasta"))
     ref_seq=str(ref.seq)
     gen_list = get_genotypes(fastq, ref_seq, 
                              umi_size=umi_size, 
                              quality_threshold=quality_threshold, 
                              ignore_pos=ignore_pos,
-                             reads_thr=reads_thr)
+                             reads_thr=reads_thr,
+                             save_umi_data=save_umi_data)
     
     with open(output,"w") as handle:
             for g,n in gen_list:
