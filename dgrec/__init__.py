346d988fac25b8b6e4d256e925731718de11fa69
David <dbikard@pasteur.fr>
2024-03-05 16:14:18 +0100

    refactoring and cli

--- dgrec/__init__.py
+++ dgrec/__init__.py
@@ -1,7 +1,7 @@
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 
 from .example_data import get_example_data_dir
-from .core import get_genotypes
+from .genotypes import get_genotypes
 from .plotting import plot_mutations
 from .encoding import encode_tr_list
 from .predictions import score,score_list
