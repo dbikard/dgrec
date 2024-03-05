c6d49855dd9858db9b924590097b3e77462b5936
David <dbikard@pasteur.fr>
2024-03-05 16:15:50 +0100

    get_genotypes_paired at the package root

--- dgrec/__init__.py
+++ dgrec/__init__.py
@@ -2,6 +2,7 @@ __version__ = "0.0.7"
 
 from .example_data import get_example_data_dir
 from .genotypes import get_genotypes
+from .genotypes_paired import get_genotypes_paired
 from .plotting import plot_mutations
 from .encoding import encode_tr_list
 from .predictions import score,score_list
