6ef61591f481dbc503a0b83cf1dd7afa873dd5c6
David <dbikard@pasteur.fr>
2024-07-24 12:13:12 +0200

    removed encode_tr_list import from __init__.py

--- dgrec/__init__.py
+++ dgrec/__init__.py
@@ -1,8 +1,8 @@
-__version__ = "0.0.10"
+__version__ = "0.0.11"
 
 from .example_data import get_example_data_dir
 from .genotypes import get_genotypes
 from .genotypes_paired import get_genotypes_paired
 from .plotting import plot_mutations
-from .encoding import encode_tr_list
+#from .encoding import encode_tr_list
 from .predictions import score,score_list
