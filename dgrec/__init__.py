0956f16c0ac1444604399ee05efb3b825fe2eddd
dbikard <dbikard@gmail.com>
2025-05-16 15:33:30 +0200

    library_size_estimation update

--- dgrec/__init__.py
+++ dgrec/__init__.py
@@ -6,3 +6,4 @@ from .genotypes_paired import get_genotypes_paired
 from .plotting import plot_mutations
 #from .encoding import encode_tr_list
 from .predictions import score,score_list
+from . import analysis, utils
