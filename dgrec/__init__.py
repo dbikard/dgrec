f6b37ba31cab8ff2ce555ad481a14df0f7ccd001
Leo Regnier <leo.regnier.pro@outlook.com>
2025-07-01 11:28:29 +0200

    LSTM 9

--- dgrec/__init__.py
+++ dgrec/__init__.py
@@ -4,4 +4,3 @@ from .example_data import get_example_data_dir
 from .genotypes import get_genotypes
 from .genotypes_paired import get_genotypes_paired
 from .plotting import plot_mutations
-from .LSTM import *
