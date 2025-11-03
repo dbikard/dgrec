1cf4461097812c5cfc62ca04046f0925fa66154a
Leo Regnier <leo.regnier.pro@outlook.com>
2025-11-03 11:09:36 +0100

    Added the encoding and predictions

--- dgrec/__init__.py
+++ dgrec/__init__.py
@@ -4,6 +4,6 @@ from .example_data import get_example_data_dir
 from .genotypes import get_genotypes
 from .genotypes_paired import get_genotypes_paired
 from .plotting import plot_mutations
-from .encoding_bis import encode_tr_list
-from .predictions_bis import score,score_list
+from .encoding import encode_tr_list
+from .predictions import score,score_list
 from . import utils, library_size
---
+++ dgrec/model_parameters/__init__.py
@@ -0,0 +1,6 @@
+import os
+def get_model_parameters_dir():
+    """
+    Return the path to the example_data directory.
+    """
+    return os.path.join(os.path.dirname(__file__))
