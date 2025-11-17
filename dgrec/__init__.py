a2d69b886bb74ceee65a83fd0f505dc41a132ccb
Leo Regnier <leo.regnier.pro@outlook.com>
2025-11-17 15:55:07 +0100

    Add the rate model

--- dgrec/__init__.py
+++ dgrec/__init__.py
@@ -4,5 +4,6 @@ from .example_data import get_example_data_dir
 from .genotypes import get_genotypes
 from .genotypes_paired import get_genotypes_paired
 from .plotting import plot_mutations
-from .predictions import score,score_list
+from .predictions import score,score_list,DGR_percentage,DGR_percentage_list
 from .encoding import encode_tr_list
+
